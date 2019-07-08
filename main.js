0f730f78510c491cef7c932211ea60e84150980e
piotr kacprzycki <piotrek634@wp.pl>
2019-07-08 20:48:11 +0200

    new button function js

--- main.js
+++ main.js
@@ -7,3 +7,21 @@ console.log(age);
 
 
 console.log(`Nazywam sie ${name} i mam lat ${age}`);
+
+const button = document.querySelector('.header__button--js');
+console.log(button)
+
+
+
+
+
+
+
+button.addEventListener('click', (e) => {
+
+    const header = document.querySelector('.header__title--js')
+
+    header.innerHTML = 'klik,klik';
+
+
+});
